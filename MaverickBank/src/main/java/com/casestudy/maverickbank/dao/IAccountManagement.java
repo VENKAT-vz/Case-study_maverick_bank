32942a73258d4395c22f75c8b4d45f2ad01e3174
VENKATESH A <85281029+VENKAT-vz@users.noreply.github.com>
2024-08-26 08:41:54 +0530

    Delete MaverickBank directory

--- MaverickBank/src/main/java/com/casestudy/maverickbank/dao/IAccountManagement.java
+++
@@ -1,9 +0,0 @@
-package com.casestudy.maverickbank.dao;
-
-import com.casestudy.maverickbank.model.Accounts;
-
-public interface IAccountManagement {
-	Accounts createAccount(String accountType, double initialDeposit, String branchName, String ifscCode);
-    void closeAccount(Accounts account);
-
-}
